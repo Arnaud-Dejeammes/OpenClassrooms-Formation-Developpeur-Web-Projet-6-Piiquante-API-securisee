1c1d02bda8f325e4be8b9926108287e3651a5b49
Arnaud-Dejeammes <arnauddejeammes@gmail.com>
2022-10-08 18:49:47 +0200

    Code pour la stabilité du serveur, notamment avec l'ajout d'un gestionnaire d'erreurs

--- backend/server.js
+++ backend/server.js
@@ -4,10 +4,56 @@
 // Indication des chemins d'accès non nécessaires pour les modules comme avec import
 const http = require("http");
 
-// Serveur
-const server = http.createServer((request, response) => {
-    response.end("Réponse test"); // Réponse type string
-});
+// Importation de l'application
+const app = require("./app");
+
+// Fonction de determination du port : string ou number
+const normalizePort = value => {
+    const port = parseInt(value, 10);
+
+    if (isNanN(port)) {
+        return value;
+    }
+
+    if (port >= 0) {
+        return port;
+    }
+    return false
+};
 
 // Variable environnement ou port par défaut
-server.listen(process.env.PORT || 3000);
+const port = normalizePort(process.env.PORT || "3000");
+app.set("port", port)
+
+// Gestion des erreurs
+const errorHandler = error => {
+    if (error.syscall !== "listen") {
+        throw error;
+    }
+
+    const address = server.address(); // Vérifier la nécessité de la répétition
+    const bind = typeof address === "string" ? "pipe " + address : "port " + port;
+    switch (error.code) {
+        case "EACCES":
+            console.error(bind + " requires elevated privileges.");
+            process.exit(1);
+            break;
+        case "EADDRINUSE":
+            console.error(bind + " is already in use.");
+            process.exit(1);
+        default:
+            throw error;
+    };
+};
+
+// Serveur
+const server = http.createServer(app); // Renvoi aux requests et responses contenues dans les middlewares
+
+server.on("error", errorHandler);
+server.on("listening", () => {
+    const address = server.address();
+    const bind = typeof address === "string" ? "pipe " + address : "port " + port;
+    console.log("Listening on " + bind);
+});
+
+server.listen(port);
