c15f788fb4b71bb4d9910fe6ec56c7ca94052381
Arnaud-Dejeammes <arnauddejeammes@gmail.com>
2022-10-15 20:28:06 +0200

    Mise en place des premiers éléments pour les middlewares de la ressource User

---
+++ backend/controllers/user.js
@@ -0,0 +1,10 @@
+const User = require("../models/user.js");
+
+exports.signUpUser = (request, response, next) => {
+    
+};
+
+exports.logInUser = (request, response, next) => {
+    User.findOne()
+    
+};
