a61d6fb9ffb59f4b95151196d1b246ae6e41a3ab
Arnaud-Dejeammes <arnauddejeammes@gmail.com>
2022-10-17 15:42:26 +0200

    Neutralisation de l'ancienne version du serveur

--- backend/server.js
+++ backend/server.js
@@ -52,60 +52,60 @@ module.exports = server; // module.exports = app;
 //!!!Ancienne version en corrélation avec app.js!!!/
 // Importation du package http de Node.js avec la méthode require()
 // Indication des chemins d'accès non nécessaires pour les modules comme avec import
-const http = require("http");
+// const http = require("http");
 
-// Importation de l'application
-const app = require("./app");
+// // Importation de l'application
+// const app = require("./app");
 
-// Fonction de determination du port : string ou number
-const normalizePort = value => {
-    const port = parseInt(value, 10);
+// // Fonction de determination du port : string ou number
+// const normalizePort = value => {
+//     const port = parseInt(value, 10);
 
-    if (isNaN(port)) {
-        return value;
-    }
+//     if (isNaN(port)) {
+//         return value;
+//     }
 
-    if (port >= 0) {
-        return port;
-    }
-    return false
-};
+//     if (port >= 0) {
+//         return port;
+//     }
+//     return false
+// };
 
-// Variable environnement ou port par défaut
-const port = normalizePort(process.env.PORT || "3000");
-app.set("port", port)
+// // Variable environnement ou port par défaut
+// const port = normalizePort(process.env.PORT || "3000");
+// app.set("port", port)
 
-// Gestion des erreurs
-const errorHandler = error => {
-    if (error.syscall !== "listen") {
-        throw error;
-    }
+// // Gestion des erreurs
+// const errorHandler = error => {
+//     if (error.syscall !== "listen") {
+//         throw error;
+//     }
 
-    const address = server.address(); // Vérifier la nécessité de la répétition
-    const bind = typeof address === "string" ? "pipe " + address : "port " + port;
-    switch (error.code) {
-        case "EACCES":
-            console.error(bind + " requires elevated privileges.");
-            process.exit(1);
-            break;
-        case "EADDRINUSE":
-            console.error(bind + " is already in use.");
-            process.exit(1);
-        default:
-            throw error;
-    };
-};
+//     const address = server.address(); // Vérifier la nécessité de la répétition
+//     const bind = typeof address === "string" ? "pipe " + address : "port " + port;
+//     switch (error.code) {
+//         case "EACCES":
+//             console.error(bind + " requires elevated privileges.");
+//             process.exit(1);
+//             break;
+//         case "EADDRINUSE":
+//             console.error(bind + " is already in use.");
+//             process.exit(1);
+//         default:
+//             throw error;
+//     };
+// };
 
-// Serveur
-const server = http.createServer(app); // Renvoi aux requests et responses contenues dans les middlewares
+// // Serveur
+// const server = http.createServer(app); // Renvoi aux requests et responses contenues dans les middlewares
 
-server.on("error", errorHandler);
-server.on("listening", () => {
-    const address = server.address();
-    const bind = typeof address === "string" ? "pipe " + address : "port " + port;
-    console.log("Listening on " + bind);
-});
+// server.on("error", errorHandler);
+// server.on("listening", () => {
+//     const address = server.address();
+//     const bind = typeof address === "string" ? "pipe " + address : "port " + port;
+//     console.log("Listening on " + bind);
+// });
 
-server.listen(port, () => {
-    console.log(`Server running on port ${process.env.PORT}.`)
-});
+// server.listen(port, () => {
+//     console.log(`Server running on port ${process.env.PORT}.`)
+// });
