4e4bf8ccfdaa5b5fe43f9cb1b37a8b9d6e778791
Arnaud-Dejeammes <arnauddejeammes@gmail.com>
2022-10-24 13:01:34 +0200

    Mise en place de bcrypt pour le signup

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,10 +1,21 @@
+const bcrypt =require("bcrypt")
+
 const User = require("../models/user");
 
 exports.signUpUser = (request, response, next) => {
-    
+    bcrypt.hash(request.body.password, process.env.BCRYPT_SALT_ROUND) // bcrypt.hash(password, parseInt(process.env.BCRYPT_SALT_ROUND))
+        .then(hash => {
+            const user = new User({
+                email: request.body.email,
+                password: hash
+            });
+            user.save()
+                .then(() => response.status(201).json({message: "Utilisateur enregistré"}))
+                .catch(error => response.status(400).json({error}));
+        })
+        .catch(error => response.status(500).json({error}));
 };
 
 exports.logInUser = (request, response, next) => {
-    User.findOne()
-    
+    User.findOne()    
 };
