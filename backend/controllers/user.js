c4e3a63f7c5310535a27547d751cf650efdc9f33
Arnaud-Dejeammes <arnauddejeammes@gmail.com>
2022-10-24 14:02:18 +0200

    Mise en place de la fonction pour le login

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,8 +1,9 @@
-const bcrypt =require("bcrypt")
+const bcrypt = require("bcrypt")
 
 const User = require("../models/user");
 
-exports.signUpUser = (request, response, next) => {
+// Création d'un profil utilisateur (email + password)
+exports.signup = (request, response, next) => {
     bcrypt.hash(request.body.password, process.env.BCRYPT_SALT_ROUND) // bcrypt.hash(password, parseInt(process.env.BCRYPT_SALT_ROUND))
         .then(hash => {
             const user = new User({
@@ -16,6 +17,35 @@ exports.signUpUser = (request, response, next) => {
         .catch(error => response.status(500).json({error}));
 };
 
-exports.logInUser = (request, response, next) => {
-    User.findOne()    
+// Connexion au profil utilisateur
+exports.login = (request, response, next) => {
+    // Vérification de l'existence d'un utilisateur dans la base de données
+    User.findOne({
+        email: request.body.email
+    })        
+        .then(user => {
+            // Utilisateur inexistant
+            if (!user) {
+                return response.status(401).json({  // Unauthorized
+                    // Sécurité : aucune information ne doit laisser transparaître la présence ou l'absence d'un utilisateur dans la base de données
+                    message: "Identifiant ou mot de passe incorrect"                    
+                });
+            }
+            // Comparaison entre les mots de passe de connexion et de la base de données
+            bcrypt.compare(request.body.password, user.password)
+                .then(valid => {
+                    if (!valid) {
+                        return response.status(401).json({ // Unauthorized
+                            // Sécurité : aucune information ne doit laisser transparaître la présence ou l'absence d'un utilisateur dans la base de données
+                            message: "Identifiant ou mot de passe incorrect"
+                        });
+                    }
+                    response.status(200).json({
+                        userId: user._id,
+                        token: "TOKEN"
+                    });
+                })
+                .catch(error => response.status(500).json({error}));
+        })
+        .catch(error => response.status(500).json({error}));
 };
