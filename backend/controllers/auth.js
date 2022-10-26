387d3444b5a16bfacf8a3e98e7d2b7fedfce3280
Arnaud-Dejeammes <arnauddejeammes@gmail.com>
2022-10-27 00:24:58 +0200

    Mise en place de contrôles (console) pour trouver les erreurs. A conserver en attente de sécurisation

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -6,8 +6,6 @@ const jsonwebtoken = require("jsonwebtoken");
 
 const User = require("../models/user");
 
-// DB et User
-
 // ******************************* //
 // Connexion au profil utilisateur //
 // ******************************* //
@@ -16,19 +14,31 @@ exports.login = (request, response, next) => {
     User.findOne({
         email: request.body.email
     })        
+        .then(console.table(request.body)) // Possibilité d'interception des informations
         .then(user => {
-            // Utilisateur inexistant
+            // Utilisateur inexistant            
+            if (user) {console.log("Found")} // A supprimer
+
             if (!user) {
+                console.log("Not found"); // A supprimer
                 return response.status(401).json({  // Unauthorized
                     // Sécurité : aucune information ne doit laisser transparaître
                     // la présence ou l'absence d'un utilisateur dans la base de données
-                    message: "Identifiant ou mot de passe incorrect"                    
+                    message: "Identifiant ou mot de passe incorrect"
                 });
             }
             // Comparaison entre les mots de passe de connexion et de la base de données
             bcrypt.compare(request.body.password, user.password)
                 .then(valid => {
+                    
+                    if (valid) {
+                        console.log(user.password);
+                        console.log("Ça plane pour moi !");
+                    }; // A supprimer
+                    
                     if (!valid) {
+                        {console.log("Ça va pas ça !")}; // A supprimer
+
                         return response.status(401).json({ // Unauthorized
                             // Sécurité : aucune information ne doit laisser transparaître
                             // la présence ou l'absence d'un utilisateur dans la base de données
@@ -37,18 +47,14 @@ exports.login = (request, response, next) => {
                     }
                     response.status(200).json({
                         userId: user._id,
-                        token: jsonwebtoken.sign({
-                            userId: user._id,
-                            token: jsonwebtoken.sign(
-                                {userId: user._id},
-                                process.env.GENERATE_RANDOM_TOKEN, 
-                                {expiresIn: process.env.TOKEN_DURATION}
-                            )
-                        }                            
+                        token: jsonwebtoken.sign(
+                            {userId: user._id},
+                            process.env.GENERATE_RANDOM_TOKEN,
+                            {expiresIn: process.env.TOKEN_DURATION}
                         )
-                    });
-                })
+                    })                                    
+                })               
                 .catch(error => response.status(500).json({error}));
-        })
+        })        
         .catch(error => response.status(500).json({error}));
 };
--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -2,7 +2,7 @@
 // Importation des modules //
 // *********************** //
 const bcrypt = require("bcrypt")
-const jsonwebtoken = require("jsonwebtoken");
+// const jsonwebtoken = require("jsonwebtoken");
 
 const User = require("../models/user");
 
@@ -10,15 +10,17 @@ const User = require("../models/user");
 // Création d'un profil utilisateur (email + password) //
 // *************************************************** //
 exports.signup = (request, response, next) => {
-    bcrypt.hash(request.body.password, parseInt(process.env.BCRYPT_SALT_ROUND)) // bcrypt.hash(password, parseInt(process.env.BCRYPT_SALT_ROUND))
+    bcrypt.hash(request.body.password, parseInt(process.env.BCRYPT_SALT_ROUND))
         .then(hash => {
+            console.table(request.body);  // Possibilité d'interception des informations
             const user = new User({
                 email: request.body.email,
                 password: hash
             });
+            console.table(user); // Protéger l'adresse email ?
             user.save()
                 .then(() => response.status(201).json({message: "Utilisateur enregistré"}))
                 .catch(error => response.status(400).json({error}));
         })
-        .catch(error => response.status(500).json({error}));
+        .catch(error => response.status(500).json({error}));        
 };
