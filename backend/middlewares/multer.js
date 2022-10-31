7dc97079f342e729e3414a69d9739b3e081137e4
Arnaud-Dejeammes <arnauddejeammes@gmail.com>
2022-10-31 11:45:45 +0100

    Corrections et précisions

--- backend/middlewares/multer.js
+++ backend/middlewares/multer.js
@@ -3,23 +3,26 @@
 // *********************** //
 const multer = require("multer");
 
+// ********************************************* //
+// Conversion des extensions de noms de fichiers //
+// ********************************************* //
 const MIME_TYPES = {
     "image/gif": "gif",
     "image/jpg": "jpg",
     "images/jpeg": "jpg",
-    "images/png": ".png",
+    "images/png": "png",
     "image/webp": "webp"
 };
 
 // ****************************************************** //
-// Stockage des images sur le système local de fichiers   //
+// Stockage et dénomination des images dans le système local de fichiers   //
 // ****************************************************** //
 const storage = multer.diskStorage({
     destination: (request, file, callback) => {
         callback(null, "images");
-    },
+    },    
     filename: (request, file, callback) => {
-        const name = file.originalname.split(" ").join("");
+        const name = file.originalname.split(" ").join("_");
         const extension = MIME_TYPES[file.mimetype];
         callback(null, name + Date.now() + "." + extension);
     }
