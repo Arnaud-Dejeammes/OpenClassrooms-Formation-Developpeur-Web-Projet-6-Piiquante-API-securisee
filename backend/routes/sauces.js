18bf4a5f5da507d3a1a006f8310d916d9f7d7b8f
Arnaud-Dejeammes <arnauddejeammes@gmail.com>
2022-11-01 20:33:33 +0100

    Suppression des parenthèses pour permettre à la requête addSauce de fonctionner

--- backend/routes/sauces.js
+++ backend/routes/sauces.js
@@ -29,32 +29,32 @@ const sauceController = require("../controllers/sauce");
 // ************************************************** //
 // Route pour le tableau des sauces (Array of sauces) //
 // ************************************************** //
-router.get("/", (check, sauceController.getEverySauce));
+router.get("/", check, sauceController.getEverySauce);
 
 // ********************************************** //
 // Route pour une sauce spécifique (Single sauce) //
 // ********************************************** //
-router.get("/:id", (check, sauceController.getOneSauce));
+router.get("/:id", check, sauceController.getOneSauce);
 
 // ************************************************ //
 // Route pour l'ajout d'une sauce (message: String) //
 // ************************************************ //
-router.post("/", (check, multer, sauceController.addSauce));
+router.post("/", check, multer, sauceController.addSauce);
 
 // ******************************************************** //
 // Route pour la modification d'une sauce (message: String) //
 // ******************************************************** //
-router.put("/:id", (check, multer, sauceController.updateSauce));
+router.put("/:id", check, multer, sauceController.updateSauce);
 
 // ******************************************************* //
 // Route pour la suppression d'une sauce (message: String) //
 // ******************************************************* //
-router.delete("/:id", (check, sauceController.deleteSauce));
+router.delete("/:id", check, sauceController.deleteSauce);
 
 // **************************************************** //
 // Route pour l'appréciation d'une sauce (like/dislike) //
 // **************************************************** //
-router.post("/:id/like", (check, sauceController.rateSauce));
+router.post("/:id/like", check, sauceController.rateSauce);
 
 // *********** //
 // Exportation //
